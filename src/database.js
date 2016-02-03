5ee376fed74589f99a81fb3d1802890e15d9c6c6
Deseteral <deseteral@gmail.com>
2016-02-03 13:47:55 +0100

    Sorting songs now takes title into account Closes #8

--- src/database.js
+++ src/database.js
@@ -65,6 +65,16 @@ export class Database {
         return 1;
       }
 
+      if (a.artist === b.artist) {
+        if (a.title < b.title) {
+          return -1;
+        }
+
+        if (a.title > b.title) {
+          return 1;
+        }
+      }
+
       return 0;
     });
   }
