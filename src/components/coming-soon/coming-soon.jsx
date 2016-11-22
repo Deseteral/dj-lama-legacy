6971a11b5867a7823587fed162759eb783489471
Michał Ferchow <deseteral@gmail.com>
2016-11-22 21:47:31 +0100

    Favicon

--- src/components/coming-soon/coming-soon.jsx
+++ src/components/coming-soon/coming-soon.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './coming-soon.less';
+import './coming-soon.less';
 
 export default class ComingSoon extends React.Component {
   render() {
@@ -7,7 +7,7 @@ export default class ComingSoon extends React.Component {
       <div className="coming-soon--container">
         <h1 className="coming-soon--title">DJ Lama</h1>
         <img className="coming-soon--logo" src="/resources/logo.svg" />
-        <h2 className="coming-soon--subtitle">Coming sooner or later...</h2>
+        <h2 className="coming-soon--subtitle">coming sooner or later...</h2>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import path from 'path';
 import express from 'express';
+import favicon from 'serve-favicon';
 import compression from 'compression';
 
 const PORT = process.env.PORT || 8080;
 const server = express();
+
 server.use(compression());
+server.use(
+  favicon(path.join(__dirname, 'public/resources/favicon.ico'))
+);
 
 server.use('/', express.static(path.join(__dirname, 'public')));
 
